dd63c694da7792fc26d68d598fb073e57834e470
ShifaMittal050 <mstrialaccforcrm1@outlook.com>
2024-10-04 17:44:11 +0530

    Update ADF.tf

--- ADF.tf
+++ ADF.tf
@@ -1,31 +1,31 @@
 # Create Azure RG
-resource "azurerm_resource_group" "QuickCloudPOCsADF" {
-  name     = "QuickCloudPOCsADF-RG"
+resource "azurerm_resource_group" "ADFPOC" {
+  name     = "RG-ADFPOC"
   location = "northeurope"
 }
 
 # Create Azure Data factory
-resource "azurerm_data_factory" "QuickCloudPOCsADF" {
-  name                = "QuickCloudPOCsADF"
-  location            = azurerm_resource_group.QuickCloudPOCsADF.location
-  resource_group_name = azurerm_resource_group.QuickCloudPOCsADF.name
+resource "azurerm_data_factory" "ADFPOC" {
+  name                = "ADFPOC"
+  location            = azurerm_resource_group.ADFPOC.location
+  resource_group_name = azurerm_resource_group.ADFPOC.name
 }
 
 # Create ADF linked web services
-resource "azurerm_data_factory_linked_service_web" "QuickCloudPOCsADF" {
-  name                = "QuickCloudPOCsADFlinkedservice"
-  resource_group_name = azurerm_resource_group.QuickCloudPOCsADF.name
-  data_factory_name   = azurerm_data_factory.QuickCloudPOCsADF.name
+resource "azurerm_data_factory_linked_service_web" "ADFPOC" {
+  name                = "ADFPOClinkedservice"
+  resource_group_name = azurerm_resource_group.ADFPOC.name
+  data_factory_name   = azurerm_data_factory.ADFPOC.name
   authentication_type = "Anonymous"
   url                 = "https://www.bing.com"
 }
 
 # Create ADF data set http
-resource "azurerm_data_factory_dataset_http" "QuickCloudPOCsADF" {
-  name                = "QuickCloudPOCsADFDataSet"
-  resource_group_name = azurerm_resource_group.QuickCloudPOCsADF.name
-  data_factory_name   = azurerm_data_factory.QuickCloudPOCsADF.name
-  linked_service_name = azurerm_data_factory_linked_service_web.QuickCloudPOCsADF.name
+resource "azurerm_data_factory_dataset_http" "ADFPOC" {
+  name                = "ADFPOCDataSet"
+  resource_group_name = azurerm_resource_group.ADFPOC.name
+  data_factory_name   = azurerm_data_factory.ADFPOC.name
+  linked_service_name = azurerm_data_factory_linked_service_web.ADFPOC.name
 
   relative_url   = "http://www.bing.com"
   request_body   = "foo=bar"
