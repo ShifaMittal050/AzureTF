4f0754fb0561be25cd6a02765ae75b72d3f99912
ShifaMittal050 <mstrialaccforcrm1@outlook.com>
2024-10-07 14:44:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # ARM provider block -rekhu
 provider "azurerm" {
-  version = "~>2.0"
+  version = "=3.0.0"
   features {}
 }
 # Terraform backend configuration block -precreated
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cloudquickpocs"
+    resource_group_name  = "RG-Test1"
     storage_account_name = "ccpsazuretf0001"
     container_name       = "ccpterraformstatefile"
     key                  = "ccpsterraform.tfstate"
